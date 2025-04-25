8a0db0fde247e39e38d69843048803c0af1f04ef
abdallah <abdallahkokash@outlook.com>
2025-04-25 22:58:10 +0300

    night

--- website/auth.py
+++ website/auth.py
@@ -1,6 +1,8 @@
-from flask import Blueprint, render_template, request, flash,redirect,url_for
+from flask import Blueprint, render_template, request, flash, redirect, url_for
 from .models import Employee
+from . import db
 from flask_login import login_user, logout_user, login_required, current_user
+
 auth = Blueprint('auth', __name__)
 
 
@@ -18,7 +20,38 @@ def login():
                 flash('Incorrect password', 'error')
         else:
             flash('Email does not exist.', 'error')
-    return render_template("login.html")
+    return render_template("login.html", user=current_user)
+
+
+@auth.route('/add_employee', methods=['GET', 'POST'])
+def hire():
+    if request.method == "POST":
+        ssn = request.form.get('SSN')
+        empName = request.form.get('empName')
+        empPhone = request.form.get('empPhoneNumber')
+        empAddress = request.form.get('empAddress')
+        empDateOfBirth = request.form.get('empDateOfBirth')
+        empEmail = request.form.get('empEmail')
+        empPass = request.form.get('empPass')
+        if len(ssn) != 9:
+            flash('Incorrect SSN', 'error')
+        elif len(empName) < 3:
+            flash('Incorrect Name', 'error')
+        elif len(empPhone) != 10:
+            flash('Incorrect Phone Number', 'error')
+        elif len(empAddress) < 5:
+            flash('Incorrect Address', 'error')
+        elif len(empEmail) < 8:
+            flash('Incorrect Email', 'error')
+        elif len(empPass) < 3:
+            flash('Incorrect Password', 'error')
+        else:
+            newEmp = Employee(SSN=ssn, Emp_Name=empName, Phone_Number=empPhone, Address=empAddress, Email=empEmail, Pass=empPass, Date_Of_Birth=empDateOfBirth)
+            db.session.add(newEmp)
+            db.session.commit()
+            flash('Successfully Registered', 'success')
+
+    return render_template("hire.html", user=current_user)
 
 
 @auth.route('/logout')
