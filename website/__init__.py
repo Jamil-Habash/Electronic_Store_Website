ec06e80f3d836be2e39514ccdaa0b78b99e8902c
abdallah <abdallahkokash@outlook.com>
2025-04-25 20:42:04 +0300

    Initial commit

---
+++ website/__init__.py
@@ -0,0 +1,29 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+
+db = SQLAlchemy()
+
+
+def create_app():
+    app = Flask(__name__)
+    app.config['SECRET_KEY'] = '1234Abd'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:AbdallahK123@localhost/electronic_store'
+    db.init_app(app)
+
+    from .views import views
+    from .auth import auth
+    app.register_blueprint(views, url_prefix='/')
+    app.register_blueprint(auth, url_prefix='/')
+
+    from .models import Employee
+
+    login_manager = LoginManager()
+    login_manager.login_view = 'auth.login'
+    login_manager.init_app(app)
+
+    @login_manager.user_loader
+    def load_user(Employee_ID):
+        return Employee.query.get(int(Employee_ID))
+
+    return app
---
+++ website/auth.py
@@ -0,0 +1,28 @@
+from flask import Blueprint, render_template, request, flash,redirect,url_for
+from .models import Employee
+from flask_login import login_user, logout_user, login_required, current_user
+auth = Blueprint('auth', __name__)
+
+
+@auth.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == "POST":
+        email = request.form.get('email')
+        password = request.form.get('password')
+        user = Employee.query.filter_by(Email=email).first()
+        if user:
+            if user.Pass == password:
+                flash('Logged in successfully', 'success')
+                login_user(user, remember=True)
+            else:
+                flash('Incorrect password', 'error')
+        else:
+            flash('Email does not exist.', 'error')
+    return render_template("login.html")
+
+
+@auth.route('/logout')
+@login_required
+def log_out():
+    logout_user()
+    return redirect(url_for('auth.login'))
