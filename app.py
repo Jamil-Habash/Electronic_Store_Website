8ff3c3722dc22971dc7b123f5c3a4c3de0188e60
Jamil-Habash <168636758+Jamil-Habash@users.noreply.github.com>
2025-06-07 11:27:50 +0300

    full option

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
 from website import create_app
+from flask import session,g
 
 app = create_app()
+
+@app.before_request
+def before_request():
+    cart = session.get('cart', {})
+    g.cart_count = sum(cart.values()) if cart else 0
 
 if __name__ == '__main__':
     app.run(debug=True)
